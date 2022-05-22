6637100c0fb77fb7ff36af8eb4415a8ef1067717
Sellways <kenue.edvard2004@gmail.com>
2022-05-22 19:15:17 +0500

    профиль

--- main.py
+++ main.py
@@ -15,30 +15,35 @@ def group_list(corpus: str, course: str):
     return group_list      
 
 def create_keyboard(keyboard_array):
-    if keyboard_array == False:
-        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        button = 0
-        if len(keyboard_array) > 8:
-            max_button = 3
-        elif 'Своё расписание' in keyboard_array:
-            max_button = 2
-        else:
-            max_button = 4
-        for i in keyboard_array:
-            if i != 'Назад':
-                if button < max_button:
-                    keyboard.add (i)
-                else:
-                    keyboard.row()
-                    keyboard.add (i)
-                    button = 0
-            else:
-                keyboard.row()
-                keyboard.add (i)
-            button += 1
-    else:
+    if keyboard_array is False:
         keyboard = types.ReplyKeyboardRemove()
+    else:
+        keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=4)
+        if 'Назад' in keyboard_array:
+            keyboard.add(*keyboard_array[:-1])
+            keyboard.add(keyboard_array[-1])
+        elif 'Своё расписание' in keyboard_array:
+            keyboard.add(*keyboard_array[:2])
+            keyboard.add(*keyboard_array[2:])
+        else:
+            keyboard.add(*keyboard_array)
     return keyboard
+
+def switch_trigger(position:str, next:str):
+    Trigger[position] = False
+    Trigger[next] = True
+
+def course_chain (user_group: str):
+#   // цепочки курсов
+    chain = []
+    data_list = get('https://bot-t-s.nttek.ru/rest-api/available').json()
+    rasp = get(f'https://bot-t-s.nttek.ru/rest-api/group/{data_list[0]}').json()
+    for i in rasp:
+        for j in rasp[i]:
+            if j[1:] == user_group[1:]:
+                chain.append(j)
+    chain.remove(user_group)
+    return chain
 
 def data_base(sql:str, value: tuple = ()):
 #   // отправка запросов к базе данных
@@ -84,67 +89,158 @@ async def reg_user(message: types.Message):
 
 #   //  основной функционал //
 
-@dp.message_handler(lambda message: Trigger['Main'] and message.text in menu_key)
+@dp.message_handler(lambda message: Trigger['Main'])
 async def menu(message: types.Message):
-    if message.text == 'Расписание':
-        await message.answer('Расписание')
-    elif message.text == 'Своё расписание':
+    if message.text.capitalize() == 'Расписание':
+        switch_trigger('Main', 'Rasp')
+        await message.answer('Расписание', reply_markup=create_keyboard(rasp_key))
+    elif message.text.capitalize() == 'Своё расписание':
         await message.answer('Своё расписание')
-    elif message.text == 'Профиль':
-        await message.answer('Профиль')
-    elif message.text == 'Админ':
-        await message.answer('Админ')
+    elif message.text.capitalize() == 'Профиль':
+        msg = f"Ваш профиль\n\nРоль: {cache_dict[message.from_user.id][1]}\n"
+        if cache_dict[message.from_user.id][1] == 'Студент':
+            profile_key = profile[:]
+            del profile_key[2]
+            msg += f'Группа {cache_dict[message.from_user.id][4]}'
+        else:
+            profile_key = profile[:]
+            del profile_key[1]
+            msg += f'Фамилия {cache_dict[message.from_user.id][2]}'
+        if cache_dict[message.from_user.id][5]:
+            msg += '\n\nАдмин'
+        switch_trigger('Main', 'Profile')
+        await message.answer(msg, reply_markup=create_keyboard(profile_key))
+    elif message.text.capitalize() == 'Админ':
+        switch_trigger('Main', 'Admin')
+        await message.answer('Админ', reply_markup=create_keyboard(admin_key))
+
+@dp.message_handler(lambda message: Trigger['Rasp'])
+async def menu_rasp(message: types.Message):
+    if message.text.capitalize() == '':
+        pass
+    elif message.text.capitalize() == 'Назад':
+        switch_trigger('Rasp', 'Main')
+        await message.answer('Главное меню', reply_markup = create_keyboard(menu_key))
+
+
+@dp.message_handler(lambda message: Trigger['Profile'])
+async def menu_profile(message: types.Message):
+    if message.text.capitalize() == 'Сбросить профиль':
+        Trigger['Profile'] = False
+        data_base('DELETE FROM users WHERE id = ?', (message.from_user.id, ))
+        cache_dict[message.from_user.id] = 1
+        await message.answer('Выберите роль', reply_markup=create_keyboard(role))
+    elif message.text.capitalize() == 'Изменить курс' and cache_dict[message.from_user.id][1] == 'Студент':
+        switch_trigger('Profile', 'Change_course')
+        memory.append(course_chain(cache_dict[message.from_user.id][4]))
+        await message.answer('Выберите курс', reply_markup=create_keyboard(memory[0]))
+    elif message.text.capitalize() == 'Изменить фамилию' and cache_dict[message.from_user.id][1] == 'Преподаватель':
+        switch_trigger('Profile', 'Change_surname')
+        await message.answer('Введите фамилию', reply_markup=create_keyboard(False))
+    elif message.text.capitalize() == 'Назад':
+        switch_trigger('Profile', 'Main')
+        await message.answer('Главное меню', reply_markup = create_keyboard(menu_key))
+
+@dp.message_handler(lambda message: Trigger['Change_course'])
+async def change_course(message: types.Message):
+    if message.text.upper() in memory[0]:
+        cache_dict[message.from_user.id][4] = message.text.upper()
+        data_base(
+            "UPDATE users SET 'group' = ? WHERE id = ?",
+            (message.text.upper(), message.from_user.id))
+        memory.clear()
+        switch_trigger('Change_course', 'Main')
+        await message.answer('Главное меню', reply_markup=create_keyboard(menu_key))
+
+@dp.message_handler(lambda message: Trigger['Change_surname'])
+async def change_surname(message: types.Message):
+    if not Trigger['Send']:
+        memory.append(message.text.capitalize())
+        Trigger['Send'] = True
+        await message.answer(
+            f'Ваша фамилия: {message.text.capitalize()}. Все верно?',
+            reply_markup=create_keyboard(['Да', 'Нет']))
+    else:
+        if message.text.capitalize() in ['Да', 'Нет']:
+            if message.text.capitalize() == 'Да':
+                cache_dict[message.from_user.id][2] = memory[0]
+                Trigger['Send'] = False
+                data_base("UPDATE users SET 'last_name' = ? WHERE id = ?", (memory[0], message.from_user.id))
+                switch_trigger('Change_surname', 'Main')
+                memory.clear()
+                await message.answer('Главное меню', reply_markup=create_keyboard(menu_key))
+            else:
+                del memory[0]
+                Trigger['Send'] = False
+                await message.answer('Введите фамилию ещё раз', reply_markup=create_keyboard(False))
+
+@dp.message_handler(lambda message: Trigger['Admin'])
+async def menu_admin(message: types.Message):
+    if message.text.capitalize() == '':
+        pass
+    elif message.text.capitalize() == 'Назад':
+        switch_trigger('Admin', 'Main')
+        await message.answer('Главное меню', reply_markup = create_keyboard(menu_key))
 
 #   // регистрация  //
 
-@dp.message_handler(lambda message:cache_dict[message['from']['id']] == 1 and message.text.capitalize() in role)
+@dp.message_handler(lambda message:cache_dict[message.from_user.id] == 1)
 async def reg_role(message: types.Message):
-    cache_dict[message['from']['id']] = 2
-    memory.append(message.text.capitalize())
-    if message.text.capitalize() == 'Студент':
-        await message.answer('Выберите корпус')
-    else:
-        await message.answer('Введите свою фамилию')
+    if message.text.capitalize() in role:
+        cache_dict[message.from_user.id] = 2
+        memory.append(message.text.capitalize())
+        if message.text.capitalize() == 'Студент':
+            await message.answer('Выберите корпус', reply_markup=create_keyboard(corpus))
+        else:
+            await message.answer('Введите свою фамилию', reply_markup=create_keyboard(False))
 
-@dp.message_handler(lambda message: cache_dict[message['from']['id']] == 2)
+@dp.message_handler(lambda message: cache_dict[message.from_user.id] == 2)
 async def reg_corpus(message: types.Message):
     if memory[0] == 'Студент':
         if message.text.capitalize() in corpus:
-            cache_dict[message['from']['id']] = 3
+            cache_dict[message.from_user.id] = 3
             memory.append(message.text.capitalize())
-            await message.answer('Выберите курс')
+            await message.answer('Выберите курс', reply_markup=create_keyboard(course))
     else:
         if not Trigger['Send']:
             memory.append(message.text.capitalize())
             Trigger['Send'] = True
-            await message.answer(f'Ваша фамилия: {message.text.capitalize()}. Все верно?')
+            await message.answer(
+                f'Ваша фамилия: {message.text.capitalize()}. Все верно?',
+                reply_markup=create_keyboard(['Да', 'Нет']))
         else:
             if message.text.capitalize() in ['Да', 'Нет']:
                 if message.text.capitalize() == 'Да':
-                    cache_dict[message['from']['id']] = 5
-                    await message.answer(f'Вы хотете завершить регистрацию с этими данными?\n\nРоль: {memory[0]}\nФамилия: {memory[1]}')
+                    cache_dict[message.from_user.id] = 5
+                    Trigger['Send'] = False
+                    await message.answer(
+                        f'Вы хотете завершить регистрацию с этими данными?\n\nРоль: {memory[0]}\nФамилия: {memory[1]}')
                 else:
                     del memory[1]
                     Trigger['Send'] = False
-                    await message.answer('Введите фамилию ещё раз')
+                    await message.answer('Введите фамилию ещё раз', reply_markup=create_keyboard(False))
 
-@dp.message_handler(lambda message: cache_dict[message['from']['id']] == 3 and message.text in course)
+@dp.message_handler(lambda message: cache_dict[message.from_user.id] == 3)
 async def reg_course (message: types.Message):
-    memory.append(message.text)
-    memory.append(group_list(memory[1], message.text))
-    cache_dict[message['from']['id']] = 4
-    await message.answer(f'Выберите группу\n\n{memory[3]}')
+    if message.text in course:
+        memory.append(message.text)
+        memory.append(group_list(memory[1], message.text))
+        cache_dict[message.from_user.id] = 4
+        await message.answer(f'Выберите группу', reply_markup=create_keyboard(memory[3]))
 
-@dp.message_handler(lambda message: cache_dict[message['from']['id']] == 4 and message.text.upper() in memory[3])
+@dp.message_handler(lambda message: cache_dict[message.from_user.id] == 4)
 async def reg_group(message: types.Message):
-    memory[3] = message.text.upper()
-    cache_dict[message['from']['id']] = 5
-    await message.answer(f'Вы хотете завершить регистрацию с этими данными?\n\nРоль: {memory[0]}\nГруппа: {memory[3]}')
+    if message.text.upper() in memory[3]:
+        memory[3] = message.text.upper()
+        cache_dict[message.from_user.id] = 5
+        await message.answer(
+            f'Вы хотете завершить регистрацию с этими данными?\n\nРоль: {memory[0]}\nГруппа: {memory[3]}',
+            reply_markup=create_keyboard(['Да', 'Нет']))
 
-@dp.message_handler(lambda message: cache_dict[message['from']['id']] == 5 and message.text.capitalize() in ['Да', 'Нет'])
+@dp.message_handler(lambda message: cache_dict[message.from_user.id] == 5)
 async def reg_confirm (message: types.Message):
     if message.text.capitalize() == 'Да':
-        if message['from']['id'] in admin_list:
+        if message.from_user.id in admin_list:
             admin = True
         else:
             admin = False
@@ -156,13 +252,14 @@ async def reg_confirm (message: types.Message):
             last_name = memory[1]
             corpus = None
             group = None
-        cache_dict[message['from']['id']] = [message['from']['id'], memory[0], last_name, corpus, group, admin]
-        data_base('INSERT INTO users VALUES(?, ?, ?, ?, ?, ?)', tuple(cache_dict[message['from']['id']]))
+        cache_dict[message.from_user.id] = [message.from_user.id, memory[0], last_name, corpus, group, admin]
+        memory.clear()
+        data_base('INSERT INTO users VALUES(?, ?, ?, ?, ?, ?)', tuple(cache_dict[message.from_user.id]))
         Trigger['Main'] = True
-        await message.answer(f'гатова\n\n {cache_dict}', reply_markup=create_keyboard(menu_key))
+        await message.answer(f'гатова', reply_markup=create_keyboard(menu_key))
     else:
         memory.clear()
-        cache_dict[message['from']['id']] = 1
+        cache_dict[message.from_user.id] = 1
         await message.answer('Выберите роль', reply_markup=create_keyboard(role))
         
 
